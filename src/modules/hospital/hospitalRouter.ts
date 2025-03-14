ff1b83f24f81a558aafdf0b132bd0b2aebb04074
JAYANT ISSAR <jayantissar23@gmail.com>
2025-03-14 20:00:27 +0530

    added route to seed mock data (currently commented)

--- src/modules/hospital/hospitalRouter.ts
+++ src/modules/hospital/hospitalRouter.ts
@@ -4,6 +4,7 @@ import { HospitalController } from "./hospitalController";
 import { db } from "../../lib/db";
 import { validateRequest } from "../../middleware/validateRequest";
 import { updateBedAvailabilitySchema } from "../../schemas/hospitalSchemas";
+import { dummyHospitals } from "../../../prisma/dummyData";
 
 // Initialize services and controllers
 const hospitalService = new HospitalService(db);
@@ -16,3 +17,47 @@ export const hospitalRouter = new Hono();
 hospitalRouter.get("/", (c) => hospitalController.getAllHospitals(c));
 hospitalRouter.get("/:id", (c) => hospitalController.getHospitalById(c));
 hospitalRouter.put("/:hospitalId/beds/:bedId", validateRequest(updateBedAvailabilitySchema), (c) => hospitalController.updateBedAvailability(c)); 
+
+
+hospitalRouter.get("/seed", async (c) => {
+    console.log("Seeding dummy hospitals...");
+    try {
+        
+        await Promise.all(dummyHospitals.map(async (hospital) => {
+                let createdHospital = await db.hospital.create({
+                data:{
+                    address: hospital.address,
+                    name: hospital.name,
+                    phone: hospital.phone,
+                    latitude: hospital.location.lat,
+                    longitude: hospital.location.lng,
+
+                }
+
+            })
+
+
+            await Promise.all(hospital.beds.map(async (bed)=>{
+                await db.bedInformation.create({
+                    data:{
+                        available: bed.available,
+                        total: bed.total,
+                        type: bed.type,
+                        Hospital:{
+                            connect:{id: createdHospital.id}
+                        }
+                    }
+                })
+            }))
+            console.log("Hospital seeded:", createdHospital);
+
+        }))
+
+
+        return c.json({message: "Dummy hospitals seeded successfully"});
+        
+    } catch (error) {
+        console.error("Error seeding dummy hospitals:", error);
+        return c.json({message: "Error seeding dummy hospitals"}, 500);
+    }
+});
