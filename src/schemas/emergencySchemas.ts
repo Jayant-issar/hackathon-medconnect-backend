876302d2bec68dcdd62d1417462cd86a24993c88
JAYANT ISSAR <jayantissar23@gmail.com>
2025-03-15 21:39:33 +0530

    removed unused comments

--- src/schemas/emergencySchemas.ts
+++ src/schemas/emergencySchemas.ts
@@ -4,11 +4,11 @@ export const createEmergencySchema = z.object({
   name: z.string().min(3, { message: "Name must be at least 3 characters long" }),
   location: z.string().min(3, { message: "Location must be specified" }),
   contactName: z.string().min(3, { message: "Contact name must be provided" }),
-  contactPhone: z.string().min(10, { message: "Contact phone must be valid" }), // Basic phone validation
-  urgencyLevel: z.string(), // Consider making this an enum validation if you have specific urgency levels
+  contactPhone: z.string().min(10, { message: "Contact phone must be valid" }), 
+  urgencyLevel: z.string(),
   description: z.string().optional(),
   hospitalName: z.string().optional(),
-  userId: z.string(), // Assuming userId is mandatory to link to a user
+  userId: z.string(), 
 });
 
 export type CreateEmergencyType = z.infer<typeof createEmergencySchema>;
